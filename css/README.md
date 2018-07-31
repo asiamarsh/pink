b0a162382c27336cb71401ec73881aa58c5d1307
Настя <asiamarsh96@gmail.com>
2018-07-31 22:16:27 +0300

    надо настроить сборщик

--- css/README.md
+++
@@ -1,1 +0,0 @@
-# pink git init git add README.md git commit -m first commit git remote add origin https://github.com/asiamarsh/pink.git git push -u origin master
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,50 @@
-"use strict";
+const gulp = require('gulp');
+const less = require('gulp-less');
+const plumber = require("gulp-plumber");
+const csso = require('gulp-csso');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
 
-var gulp = require("gulp");
-var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-
-gulp.task("less",function(){
-  gulp.src("less/style.less")
-    .pipe(plumber())
+gulp.task('less', () => gulp.src('src/style/main.less')
+  .pipe(plumber())
+  .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+  .pipe(sourcemaps.write())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('src/style/css'))
+  .pipe(browserSync.reload({ stream: true }))
+);
+
+gulp.task('browser-sync', () => browserSync({
+  server: {
+    baseDir: 'src/'
+  },
+}));
+
+gulp.task('watch', ['browser-sync', 'less'], () => {
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/style/**/*.less', ['less']);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
 });
 
-gulp.task("server",["style"], function(){
-  server.init({
-    server:".",
-    notify:false,
-    open:true,
-    cors:true,
-    ui:false
-  });
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change",server.reload);
+gulp.task('clean', () => del.sync('dist'));
+  
+gulp.task('build', ['clean', 'less'], () => {
+  const buildHtml = gulp.src('src/*.html').pipe(gulp.dest('dist'));
+
+  const buildCss = gulp.src('src/css/main.min.css').pipe(gulp.dest('dist/css'));
+
+  const buildFonts = gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+
+  const buildImg = gulp.src('src/img/**/*').pipe(gulp.dest('dist/img'));
+
+  const buildJs = gulp.src('src/js/**/*').pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('default', ['watch']);
